b78ad474371b46ba5d9008616f230499f1804fed
rahulhavale256@gmail.com <rahulhavale0@gmail.com>
2025-04-20 10:28:06 +0530

    1st commit

---
+++ src/main/java/com/projrct2/practice2/Controller.java
@@ -0,0 +1,16 @@
+package com.projrct2.practice2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    //http://localhost:8080/api/message
+    @GetMapping("/message")
+    public String message(){
+        return "Hello world";
+    }
+}
